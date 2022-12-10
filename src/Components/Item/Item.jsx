61db8db81a9c233c70aab1b7381d853759215beb
matiasdamianarevalo <matiasdamianarevalo@gmail.com>
2022-12-10 13:41:06 -0300

    arreglo

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -3,20 +3,20 @@ import "./Style.css";
 
 function Item(props) {
   return (
-    <div className="ContenedorCard">
-    <div className="card">
-      <div className="card-img">
+    <div className="Container">
+      <div className="card">
+        <div className="card-img">
         <img src={props.imageUrl} alt={props.title} />
-      </div>
-   <div className="card-detail">
-        <h3>{props.title}</h3>
+        </div>
+      <div className="card-detail">
+          <h3>{props.title}</h3>
         <span>{props.description}</span>
-        <p>$ {props.price}</p>
+          <p>$ {props.price}</p>
         <span>{props.category}</span>
       </div>
-      <ItemCount />
+        <ItemCount />
       <button>Comprar</button>
-    </div>
+      </div>
     </div>
   );
 }
